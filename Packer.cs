c108a499502898dc27ca69854fc44a5ab8a7b92d
Mokhabadi <mokhabadi@gmail.com>
2023-10-20 00:15:49 +0330

    Packer generic type instead of object

--- Packer.cs
+++ Packer.cs
@@ -8,17 +8,17 @@ namespace Lynx
     {
         static readonly JsonSerializerOptions options = new() { IncludeFields = true };
 
-        public static async Task<byte[]> Pack(object obj)
+        public static async Task<byte[]> Pack<T>(T @object)
         {
             MemoryStream stream = new();
-            await JsonSerializer.SerializeAsync(stream, obj, options);
+            await JsonSerializer.SerializeAsync(stream, @object, options);
             return stream.ToArray();
         }
 
         public static async Task<T> Unpack<T>(byte[] bytes)
         {
-            T? obj = await JsonSerializer.DeserializeAsync<T>(new MemoryStream(bytes), options);
-            return obj!;
+            T? @object = await JsonSerializer.DeserializeAsync<T>(new MemoryStream(bytes), options);
+            return @object!;
         }
     }
 }
