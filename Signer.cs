1ef5611c92a8cd997d88ab9f0a6ded342fa35a24
Mokhabadi <mokhabadi@gmail.com>
2023-10-29 18:58:32 +0330

    Signer for Sign and Verify

---
+++ Signer.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+
+namespace Lynx
+{
+    public static class Signer
+    {
+        public static byte[] Sign(byte[] key, byte[] bytes)
+        {
+            RSA RSA = RSA.Create();
+            RSA.ImportRSAPrivateKey(key, out _);
+            return RSA.SignData(bytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pss);
+        }
+
+        public static bool Verify(byte[] key, byte[] bytes, byte[] signedBytes)
+        {
+            RSA RSA = RSA.Create();
+            RSA.ImportRSAPublicKey(key, out _);
+            return RSA.VerifyData(bytes, signedBytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pss);
+        }
+    }
+}
